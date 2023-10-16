ae043f5cd1f2966c3a39c2db40090070dc0af11f
jamesjoel <jamesjoelj@gmail.com>
2023-10-16 13:54:01 +0530

    live mongodb user insert

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-require("mongoose").connect("mongodb://0.0.0.0:27017/tss12_project")
+require("mongoose").connect("mongodb+srv://jamesjoel:3NOLjUjPnAAvuAIn@cluster0.kys2siz.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     // console.log("DATA BASE CONNECTED");
 }).catch((err)=>{
